18ebbad77a5aa7292c88df519b844a81264ac85c
DurvankurSonar <113711639+DurvankurSonar@users.noreply.github.com>
2022-11-11 18:11:30 +0530

    Create 9.js

---
+++ 9.js
@@ -0,0 +1,27 @@
+<script>
+a=-5;
+b=-2;
+c=-6;
+d= 0;
+f=-1;
+if (a>b && a>c && a>d && a>f)
+{
+    alert(a);
+}
+else if (b>a && b>c && b>d && b>f)
+{
+    alert(b);
+}
+else if (c>a && c>b && c>d && c>f)
+{
+    alertg(c);
+}
+else if (d>a && d>c && d>b && d>f)
+{
+    alert(d);
+}
+else
+{
+  alert(f);
+}
+</script>
